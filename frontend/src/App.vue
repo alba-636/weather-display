34ce04f38fc69865117ab31f991f3c6efebec70a
alba-636 <29271954+alba-636@users.noreply.github.com>
2026-06-29 11:38:39 +0200

    chore: split frontend and backend

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,7 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Test from './components/Test.vue';
+
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,7 +9,7 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
 
-    test
+    <Test />
   </p>
 </template>
 
