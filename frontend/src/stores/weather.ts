b2dc17bb236d4d264c8cff88fd1473f4e1ea0fee
alba-636 <29271954+alba-636@users.noreply.github.com>
2026-07-06 20:32:44 +0200

    feat: add missing file

--- frontend/src/stores/weather.ts
+++ frontend/src/stores/weather.ts
@@ -13,8 +13,8 @@ export const useWeatherStore = defineStore('weather', () => {
         current.value = weather
     }
 
-    async function updateHourlyForecastWeather (latitude: number, longitude: number, days: number = 7) {
-        const weather = await OpenMeteoController.fetchForecast(latitude, longitude, days)
+    async function updateHourlyForecastWeather (latitude: number, longitude: number, days: number = 7, model: string | undefined | null = 'best_match') {
+        const weather = await OpenMeteoController.fetchForecast(latitude, longitude, days, model)
         if (!weather) return
 
         hourlyForecast.value = weather
